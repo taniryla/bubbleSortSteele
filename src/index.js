7216be5d626ede6337b32b47fed6e7e49d545ead
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-30 23:08:19 -0700

    swap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 function bubbleSort(arr) {
   for (let i = arr.length - 1; i < arr.length - 1; i--) {
-    for (let j = 0; j < i - 1; j++) {}
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
   }
 }
