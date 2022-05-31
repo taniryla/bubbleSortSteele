6070f5605c6639ac9ba19256b674614c5827a46c
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-30 23:03:32 -0700

    nested for loop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 function bubbleSort(arr) {
-  for (let i = arr.length - 1; i < arr.length - 1; i--) {}
+  for (let i = arr.length - 1; i < arr.length - 1; i--) {
+    for (let j = 0; j < i - 1; j++) {}
+  }
 }
