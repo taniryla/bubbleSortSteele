e5f79d48c5dc1e05a07ca7b2aaf421cb19bea733
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-30 23:32:48 -0700

    optimize the array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function bubbleSort(arr) {
-  for (let i = arr.length - 1; i < arr.length - 1; i--) {
+  for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
+      console.log(arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -10,3 +11,5 @@ function bubbleSort(arr) {
   }
   return arr;
 }
+
+bubbleSort([37, 45, 29, 8]);
